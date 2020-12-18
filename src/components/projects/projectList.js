3d8e05bb72d371ddb17df9614c2602bc0e32e65c
Shane Benlolo <benlolo1000@gmail.com>
2020-12-18 15:12:09 -0500

    abstracts projects component and adds project list object to map from

---
+++ src/components/projects/projectList.js
@@ -0,0 +1,16 @@
+import SanFranTokyo from '../../images/SanFranTokyo.png'
+
+export default {
+    projects: [
+        {
+            name: "SanFranTokyo",
+            mockUp: SanFranTokyo,
+            desc:  "SanFranTokyo is an online storefront created for a brick and mortar restaurant. Users can view, select, and purchase items from the storefront, as well as find basic info about the restaurant such as hours, and location."
+        },
+        {
+            name: "Test",
+            mockUp: SanFranTokyo,
+            desc:  "SanFranTokyo is an online storefront created for a brick and mortar restaurant. Users can view, select, and purchase items from the storefront, as well as find basic info about the restaurant such as hours, and location."
+        },
+    ]
+};
--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import '../../App.css';
-import SanFranTokyo from '../projects/SanFranTokyo'
+import projectsList from '../projects/projectList'
+import Buttons from '../buttons/Buttons'
 
 export default function Projects() {
+
+const projects = projectsList.projects
+
   return (
-    <div className='projects'>
-      <SanFranTokyo />
-    </div>
+    <>
+    {projects.map((projects)=>
+      <div className='projects'>
+        <h1 className="projectHeader">{projects.name}</h1>
+          <div className="projectDetails">
+            <img className="mockUp" src={projects.mockUp} alt="SanFranTokyo"></img>
+            <div className="right">
+              <p className="projDescription">{projects.desc}</p>
+              <Buttons />
+            </div>
+          </div>
+      </div>
+    )}
+    </>
   )
 }
